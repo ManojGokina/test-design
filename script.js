c2c2916ec0e1f1c914753e71b259055a3ad80369
manoj gokina <manojgokina@gmail.com>
2022-02-15 13:31:16 +0530

    Script Added.

--- script.js
+++ script.js
@@ -1,3 +1,63 @@
+const itemsData = [
+  { name: "Wooden Table Lamp", price: "$2,300", img: "./images/image98.png" },
+  { name: "Shaped Sofa", price: "$2899", img: "./images/image89.png" },
+  {
+    name: "European Style Sofa",
+    price: "$4890",
+    img: "./images/Frame84581.png",
+  },
+  { name: "Long Single Tool", price: "$3499", img: "./images/image95.png" },
+  { name: "Wooden Table Lamp", price: "$2,300", img: "./images/image98.png" },
+  { name: "Shaped Sofa", price: "$2899", img: "./images/image89.png" },
+  {
+    name: "European Style Sofa",
+    price: "$4890",
+    img: "./images/Frame84581.png",
+  },
+  { name: "Long Single Tool", price: "$3499", img: "./images/image95.png" },
+  { name: "Wooden Table Lamp", price: "$2,300", img: "./images/image98.png" },
+  { name: "Shaped Sofa", price: "$2899", img: "./images/image89.png" },
+  {
+    name: "European Style Sofa",
+    price: "$4890",
+    img: "./images/Frame84581.png",
+  },
+  { name: "Long Single Tool", price: "$3499", img: "./images/image95.png" },
+  { name: "Wooden Table Lamp", price: "$2,300", img: "./images/image98.png" },
+  { name: "Shaped Sofa", price: "$2899", img: "./images/image89.png" },
+  {
+    name: "European Style Sofa",
+    price: "$4890",
+    img: "./images/Frame84581.png",
+  },
+  { name: "Long Single Tool", price: "$3499", img: "./images/image95.png" },
+  { name: "Wooden Table Lamp", price: "$2,300", img: "./images/image98.png" },
+  { name: "Shaped Sofa", price: "$2899", img: "./images/image89.png" },
+  {
+    name: "European Style Sofa",
+    price: "$4890",
+    img: "./images/Frame84581.png",
+  },
+  { name: "Long Single Tool", price: "$3499", img: "./images/image95.png" },
+];
+
+function loadTableData(itemsData) {
+  const mainDiv = document.getElementById("itemBody");
+  let htmlData = "";
+
+  for (let item of itemsData) {
+    htmlData += `<div class="itemsContaner">
+       <img src=${item.img} alt="item image" class="itemImage" />
+       <h2>${item.name}</h2>
+       <h3>${item.price}</h3>
+     </div>`;
+  }
+
+  console.log(htmlData);
+  mainDiv.innerHTML = htmlData;
+}
+
+loadTableData(itemsData);
 
 var slideIndex = 1;
 showSlides(slideIndex);
